692b0043a4c28c7c4d6d3d18ac34a5692a80964c
Matt Ravenmoore <M_ravenmoore@outlook.com>
2020-09-14 19:28:21 -0700

    first try at data pull.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const { response, raw } = require('express');
 
 
 const PORT = process.env.PORT;
@@ -12,6 +13,28 @@ app. use(cors());
 
 app.get('/', (request,response) => {
   response.send('I am a Dragon')
-})
+});
+
+app.get('/location', handleLocation);
+
+
+function handleLocation(request, response) {
+  try {
+    const rawLocationData = require('./data/location.json');
+    const cityInput = request.query.city;
+    const locationData = new Location(cityInput,rawLocationData);
+    response.send(locationData);
+  }catch (error){
+    console.log('ERROR', error);
+    response.status(500).send('Join the dark side we have cookies and Errors please come back soon')
+  }
+};
+
+function Location(cityInput,rawLocationData) {
+  this.search_query = cityInput;
+  this.formatted_query = rawLocationData[0].display_name;
+  this.latitude = rawLocationData[0].lat;
+  this.longitude = rawLocationData[0].lon;
+}
 
 app.listen(PORT , () => console.log(`app is listening on : ${PORT}`));
