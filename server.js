11c498614d1a721ab2e3b6380b99f5c90fcbf404
Matt Ravenmoore <M_ravenmoore@outlook.com>
2020-09-15 23:00:36 -0700

    finished weather api incorperation

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ function handleLocation (req,res){
     superagent.get(url)
     .then(data => {
       const location = new Location(city,data.body[0])
-      console.log(data.body)
+      console.log(data.body[0])
       console.log(location)
       res.send(location)
     })
@@ -43,13 +43,19 @@ function handleLocation (req,res){
   }
 };
 
-function handleWeather(request, response) {
+function handleWeather(req,res){
   try {
-    const rawWeatherData = require('./data/weather.json');
-    const weatherArr = rawWeatherData.data.map(day => new Weather(day));
-    console.log(weatherArr)
+    let lon = req.query.longitude;
+    let lat = req.query.latitude;
+    const key = process.env.WEATHER_API_KEY;
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=8&key=${key}`;
 
-    response.send(weatherArr);
+    superagent(url)
+    .then(weatherData => {
+      console.log(weatherData.body.data[0])
+      const day = weatherData.body.data.map(day => new DailyWeather(day))
+      res.send(day)
+    })
   }catch(error){
     console.log('ERROR', error);
     response.status(500).send('Join the dark side we have cookies and Errors please come back soon')
@@ -68,10 +74,22 @@ function Location(city,data) {
   this.longitude = data.lon;
 }
 
-function Weather(day){
+function DailyWeather(day){
   this.forecast = day.weather.description;
-  this.time = day.valid_date;
+  let dateFormat = day.valid_date;
+  this.time = new Date(dateFormat).toDateString()
 }
-
+// function Trail(trail){
+// this.name= Mt. Si",
+// this.location= ": "Tanner, Washington",
+// this.length =": "6.6",
+// this.stars =": "4.4",
+// this.star_votes =": "72",
+// this.summary =": "A steep, well-maintained trail takes you atop Mt. Si with outrageous views of Puget Sound.",
+// this.trail_url =": "https://www.hikingproject.com/trail/7001016/mt-si",
+// this.conditions =": "Dry",
+// this.condition_date =": "2018-07-22",
+// this.condition_time =": "0:17:22 "
+// }
 
 app.listen(PORT , () => console.log(`app is listening on : ${PORT}`));
