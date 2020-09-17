c8b09f0c4285e8087158945a466f8fcfa3b419e8
Matt Ravenmoore <M_ravenmoore@outlook.com>
2020-09-16 23:19:17 -0700

    got locations working with db

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 ## Overview
 
-This is an api to collect data and distribute it to a fron end location information finding page.
+This is an api to collect data and distribute it to a front end location information finding page.
 
 ## Getting Started
 
@@ -17,6 +17,8 @@ you can do this individualy or as one command witn (npm i)
 * Express
 * cors
 * dotenv
+* superAgent
+* postgres
 
 ## Architecture
 
@@ -28,78 +30,20 @@ Currently using:
 * JavaScript
 * heroku
 * superAgent
+* postgres
 
 ## Change Log
 
-<!-- All times in PST-->
-
-09-14-2020 5:00pm PST  - Application now has a fully-functional express server, with heroku deployment and proof of life route.
-09-14-2020 8:00pm PST  - App can now pull data on locations from internal file
-09-14-2020 9:45pm PST  - App can now pull data on weather from internal file and parse it to usable needs
-
-09-15-2020 11:15am PST  - App Now has catch errors for 404 and 500 codes
-09-15-2020 3:45pm PST  - App can now pull data on weather from internal file via map method.
+09-14-2020 5:00pm PST - Application now has a fully-functional express server, with heroku deployment and proof of life route.
+09-14-2020 8:00pm PST - App can now pull data on locations from internal file.
+09-14-2020 9:45pm PST - App can now pull data on weather from internal file and parse it to usable needs.
+09-15-2020 11:15am PST - App Now has catch errors for 404 and 500 codes
+09-15-2020 3:45pm PST - App can now pull data on weather from internal file via map method.
 09-15-2020 11:00 PST - App can now pull data from weather and display it from external api.
-
 
 ## work log
 
-
-### Feature 1
-
-  Number and name of feature: 1 Locations
-  Estimate of time needed to complete: 45 min
-  Start time:6:30pm
-  Finish time: 8:00pm
-  Actual time needed to complete:2 hour 30 min
-
-### Feature 2
-
-  Number and name of feature: 2 weather
-  Estimate of time needed to complete: 1 hour 15 min
-  Start time:8:00pm
-  Finish time: 9:36pm
-  Actual time needed to complete:2 hour 30 min
-
-### Feature 3
-
-Number and name of feature: Error catching
-Estimate of time needed to complete: 1 hour 15 min
-Start time:10:05am
-Finish time: 10:20am
-Actual time needed to complete:15 min
-
-### Feature 4
-
-Number and name of feature: .map(Reforating)
-Estimate of time needed to complete:45 min
-Start time:3:15pm
-Finish time: 3:45pm
-Actual time needed to complete:30min
-
-### Feature 5
-
-Number and name of feature: location API
-Estimate of time needed to complete: 1 hour 15 min
-Start time:4:05am
-Finish time: 7:35am
-Actual time needed to complete:3 hours 30 min
-
-### Feature 6
-
-Number and name of feature: Weather Api
-Estimate of time needed to complete: 1 hour
-Start time:8:30pm
-Finish time: 11:00pm
-Actual time needed to complete: 3 hours 30 min
-
-### Feature 7
-
-Number and name of feature: trails Api
-Estimate of time needed to complete: 1 hour
-Start time:11:15pm
-Finish time: 11:50pm
-Actual time needed to complete: 35 min
+[See Feature log](../worklog.md)
 
 ## Credits and Collaborations
 
--- server.js
+++ server.js
@@ -5,41 +5,86 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const superagent = require('superagent');
+const pg = require('pg')
 
 
-const PORT = process.env.PORT;
 const app = express();
+const client = new pg.Client(process.env.DATABASE_URL);
 app. use(cors());
 
-app.get('/', (request,response) => {
-  response.send('I am a Dragon');
-});
+const PORT = process.env.PORT;
+
+// holder for object pre creating db save(get it working then save it)
+let locations = {};
+
+
+// proof of life;
+app.get('/', (req,res) => res.status(200).send('I am a Dragon'));
+
+// Routes
 app.get('/location',handleLocation);
 app.get('/weather', handleWeather);
 app.get('/trails',handleTrails);
+app.get('/locations-db',handleGetLocation);
 
-// catch all fails
-app.use ('*',notFoundHandler);
+
+app.use ('*',(req,res) =>{
+  res.status(404).send('This is not the Place you are Looking for Try again: Route Not found');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send(`Welcome to the DarkSide we have Cupcakes and a Server Error: ${err.message} : ${err.txt}`);
+});
+
 
 // Handler Functions
 
 function handleLocation (req,res){
-    let city = req.query.city;
-    let key = process.env.GEOCODE_API_KEY;
-    const url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+  let city = req.query.city
+  const SQLlocation = `SELECT * FROM locations WHERE city_name=$1;`;
+  let safeValues = [city]
+  client.query(SQLlocation,safeValues)
+  .then(results => {
+    if(results.rows.length > 0) {
+      console.log(`ahh yes ..${req.query.city} I have memory of such a place!`)
+      console.log('i have found your location:',results.rows[0])
+      res.status(200).json(results.rows[0]);
+    }else{
+      console.log(`uh...${req.query.city} I have NO memory of any such a place! lets check the Archives...`);
+
+      let url = `https://us1.locationiq.com/v1/search.php`;
     
-    superagent.get(url)
+      let queryObject = {
+        city: req.query.city,
+        key: process.env.GEOCODE_API_KEY,
+        format: 'json',
+        limit: 1
+    };
+    
+    superagent.get(url).query(queryObject)
     .then(data => {
-      const location = new Location(city,data.body[0]);
-      console.log(data.body[0]);
-      console.log(location);
+      const location = new Location(req.query.city,data.body[0]);
+      console.log('i have found your location:',location);
+
+      
+      const SQL = `INSERT INTO locations (city_name,formatted_name, latitude, longitude) VALUES ($1, $2, $3, $4) returning *;`;
+      
+      const safeValues = [location.search_query,location.formatted_query,location.latitude,location.longitude];
+      
+      // Run the query
+      console.log(safeValues)
+      client.query(SQL, safeValues)
+        .then(results => {
+          console.log('Sir i have saved your data',)
+        })
+        .catch(err => {throw new Error(err.message)});
       res.status(200).send(location);
     })
     .catch(err => {
-      console.error(err);
       throw new Error(err.message);
-    })
-};
+    })};
+  });
+}
 
 function handleWeather(req,res){
     let lon = req.query.longitude;
@@ -78,9 +123,26 @@ function handleTrails(req,res){
     });
 };
 
-function notFoundHandler(req,res) {
-  res.status(404).send('This isnt not the page you are looking for! plese refresh and try again. ');
+function handleGetLocation(req,res){
+  const getLocation = "SELECT * FROM locations";
+
+  client.query(getLocation)
+    .then(results => {
+      if (results.rowCount >= 1) {
+        console.log('here is the data',results)
+        res.status(200).json(results)
+      }else {
+        res.status(500).send('NOTHING HERE MAN')
+      }
+    })
+    .catch(err => {throw new Error(err.message)})
 }
+
+// Helper Functions
+function startServer (){
+  app.listen(PORT , () => console.log(`app is listening on : ${PORT}`));
+}
+
 // constructor Functions
 
 function Location(city,data) {
@@ -108,4 +170,4 @@ function TrailInfo(trail){
   this.condition_date = trail.conditionDate;
 }
 
-app.listen(PORT , () => console.log(`app is listening on : ${PORT}`));
+client.connect(startServer);
